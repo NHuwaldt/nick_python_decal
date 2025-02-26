e62e61aa63ab79ebc8a342ee41d31617a7cb1921
Nick Huwaldt <nhuwaldt@berkeley.edu>
2025-02-25 18:52:04 -0800

    adding homework2.py

--- homework2/hw2.py
+++ homework2/hw2.py
@@ -1,10 +1,21 @@
 # 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+#cd returns to top level directory
 # 2) What does ~/ mean?
+#this means the user is on the home directory
 # 3) What's the difference between and abolute and a relative path in your terminal?
+#absolute path describes the exact location from anywhere, whereas relative paths depend on current location
 # 4) What command would bring you back to your home directory?
+#just "cd" seems to work in gitbash for me!
 # 5) If you called rm ./ in your current working directory, what would it do? 
+#this would remove the file with the name following the command
 # 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+#git add stages, while commit actually adds, and then push updates the directory online
 # 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+#it should say what file is staging to be added
 # 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+#I've been fortunate to avoid bugs thus far, Physics 111B however is kicking my butt when it comes to coding
 # 9) How does cloning a repository differ from pulling from a repository?
+#cloning copies instead of moving from the repository
 # 10) Tell me a fun fact!
+#the power disipated by dipole radiation in the sky is proportional to freq**4, which would actually make our sky purple,
+# but the cones in our eyes that receive light aren't as efficient at processing purple as they are blue
